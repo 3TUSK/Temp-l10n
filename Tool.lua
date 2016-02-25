85e576e92e8bab80f71c4f2c8907a0fee0936984
Urey.X <urey.s.knowledge@gmail.com>
2016-02-25 18:09:19 -0500

    Finished update tool, close #57

--- Tool.lua
+++ Tool.lua
@@ -2,13 +2,19 @@
 -- This language file updater is licensed under CC-BY-SA 4.0 International
 -- See here for more details about license: 
 -- https://creativecommons.org/licenses/by-sa/4.0/
-
-local mode = arg[1]
-local file = arg[2]
+-- ------------------------------------------------------------------------
+-- User instruction:
+-- 1.Put corresponding en_US.lang and zh_CN.lang into same folder
+-- 2.Install lua 5.3.2
+-- 3.Put this script where there are language files mentioned in 1.
+-- 4.Run this file and you're good to go
+-- -----------------------------------------------------------------------
+-- local mode = arg[1]
+-- local file = arg[2]
 
 enUSFile = io.open("en_US.lang")
 zhCNFile = io.open("zh_CN.lang")
-outputFinal = io.open("zh_CN-merged.lang", "w")
+outputFinal = io.open("zh_CN-merged.lang", "w+")
 
 mapping = {}
 zhCN = {}
@@ -19,47 +25,75 @@ print("Language file update started, please stand by...")
 str = ''
 count = 1
 
-for s in enUSFile:lines("L") do
-	if (string.gmatch(s, "([%w%s%.]*)=.*\n")) then
-		str = string.gsub(s, "([%w%s%.]*)=.*", "%1")
---		outputFinal:write(str)
+for s in enUSFile:lines() do
+	if (string.match(s, "([%w%s%.]*)=.*")) then
+		str = string.match(s, "([%w%s%.]*)=.*")
 		mapping[count] = str
+		elseif (string.match(s, "#.*")) then
+			str = string.gsub(s, "(^\n)", "%1")
+			mapping[count] = str
 		else
-			mapping[count] = s
+			str = ""
+			mapping[count] = str
 	end
 	
 	count = count + 1
 end
 
+debug = {"Readed ", count, " lines in en_US.lang"}
+print(table.concat(debug))
+
 count = 1
 
 pair = {}
 
-for s in zhCN:lines("L") do
-	if (string.gmatch(s, "([%w%s%.]*)=.*\n")) then
+for s in zhCNFile:lines() do
+	if (string.match(s, "([%w%s%.]*)=.*")) then
 		pair = {string.gsub(s, "([%w%s%.]*)=.*", "%1"), string.gsub(s, "[%w%s%.]*=([.]*)", "%1")}
 		zhCN[count] = pair
+		count = count + 1
 	end
-	count = count + 1
 end
+
+print(count)
 
 finalPair = {}
 
 for i, v in ipairs(mapping) do
-	if (string.gmatch(v, "([%w%s%.]*)")) then
+	print(v)
+
+	if (string.match(v, "(#.*)")) then
+		tmpStr = string.gsub(v, "(^\n)", "%1")
+		finalPair = {tmpStr, "\n"}
+		outputFinal:write(table.concat(finalPair))
+	end
+
+--	if (v == "") then
+--		outputFinal:write("\n")
+--	end
+
+	if (string.match(v, "([%w%s%.]*)")) then
+		found = false
 		for j, w in ipairs(zhCN) do
 			if (v == w[1]) then
-				finalPair = {v, "=", w[2]}
+				finalPair = {v, "=", w[2], "\n"}
 				outputFinal:write(table.concat(finalPair))
+				found = true
 				break
 			end
 		end
-	else
-		outputFinal:write(v)
+		if (found == false) then
+			if not (string.sub(v, 1, 1) == "\n") then
+				if not (string.sub(v, 1, 1) == "#") then
+					finalPair = {v, "\n"}
+					outputFinal:write(table.concat(finalPair))
+				end
+			end
+		end
 	end
 end 
 
-print(string.format("Language file successfully updated with %n new lines", #enUSFile - #zhCNFile))
+print("Language file successfully updated")
 
 outputFinal:flush()
 outputFinal:close()
