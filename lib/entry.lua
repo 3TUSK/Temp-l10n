9ae00bd74cdc1805be0511b4ad7eaa1a7494cdec
3TUSK <urey.s.knowledge@gmail.com>
2016-08-10 04:21:45 +0800

    Overhaul entry.lua

--- lib/entry.lua
+++ lib/entry.lua
@@ -1,44 +1,46 @@
+-- TemporaryLocalization-lib
+-- Authored by 3TUSK, licensed under CC-BY-SA 4.0 International
+-- Yes, I have wrote Java too much and I want to do something
+
 local Entry = {}
 Entry.__index = Entry
 
-function Entry:create(key, text)
- return setmetatable({key = key, text = text, translation = text}, Entry)
+Enrty.__eq =  function (obj)
+  return self.key == obj.key && self.value == obj.value
 end
 
-function Entry:parse(keyValuePair)
- local key, text = string.match(keyValuePair, "(.+)=.+"), string.match(keyValuePair, ".+=(.*)")
- return setmetatable({key = key, text = text, translation = text}, Entry)
-end
-
-function Entry:toString()
- str = {self.key, "=", self.text}
+Entry.__tostring = function (v)
+ str = {self.key, "=", self.value}
  return table.concat(str)
 end
 
-function Entry:toString(translate)
- if translate then
-  return table.concat({self.key, "=", self.translation})
- else
-  return table.concat({self.key, "=", self.text})
- end
+function Entry:create(K, V)
+ return setmetatable(Entry, {key = K, value = V})
+end
+
+function Entry:parse(keyValuePair)
+ local K, V = string.match(keyValuePair, "(.+)=.+"), string.match(keyValuePair, ".+=(.*)")
+ return setmetatable(Entry, {key = K, value = V})
+end
+
+function Entry:toString()
+ str = {self.key, "=", self.value}
+ return table.concat(str)
 end
 
 function Entry:getKey()
  return self.key
 end
 
-function Entry:getText()
+function Entry:getValue()
  return self.text
 end
 
-function Entry:getTranslation()
- return self.translation
-end
-
-function Entry:translate(translation)
+function Entry:setValue(translation)
  self.translation = translation
 end
 
+-- TODO: override the metamethod so that we can simply use ==
 function Entry:equals(anEntry)
  return anEntry:getKey() == self.key
 end
